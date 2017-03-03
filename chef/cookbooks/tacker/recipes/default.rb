bdca9fe684c28195010eb9a80d2db055b2a099b7
root <root@crowbar.virtual.cloud.suse.de>
2017-03-03 10:51:06 +0100

    Added MySQL support
    Signed-off-by: root <root@crowbar.virtual.cloud.suse.de>

--- chef/cookbooks/tacker/recipes/default.rb
+++ chef/cookbooks/tacker/recipes/default.rb
@@ -31,62 +31,17 @@ git "/tmp/tackerclient" do
     action :sync
 end
 
-db_settings = fetch_database_settings
-include_recipe "database::client"
-include_recipe "#{db_settings[:backend_name]}::client"
-include_recipe "#{db_settings[:backend_name]}::python-client"
+db = "mysql"
 
-props = [ {'db_name' => node[:tacker][:db][:database],
-          'db_user' => node[:tacker][:db][:user],
-          'db_pass' => node[:tacker][:db][:password],
-          'db_conn_name' => 'sql_connection'  }
-]
-
-Chef::Log.info("This is the db connection #{db_settings[:connection]}")
-Chef::Log.info("This is the provider #{db_settings[:provider]}")
-
-props.each do |prop|
-  db_name = prop['db_name']
-  db_user = prop['db_user']
-  db_pass = prop['db_pass']
-  db_conn_name = prop['db_conn_name']
-  sql_address_name = prop['sql_address_name']
-
-    database "create #{db_name} tacker database" do
-        connection db_settings[:connection]
-        database_name "#{db_name}"
-        provider db_settings[:provider]
-        action :create
-        only_if { !ha_enabled || CrowbarPacemakerHelper.is_cluster_founder?(node) }
-    end
-
-    database_user "create #{db_user} user in #{db_name} tacker database" do
-        connection db_settings[:connection]
-        username "#{db_user}"
-        password "#{db_pass}"
-        host '%'
-        provider db_settings[:user_provider]
-        action :create
-        only_if { !ha_enabled || CrowbarPacemakerHelper.is_cluster_founder?(node) }
-    end
-
-    database_user "grant database access for #{db_user} user in #{db_name} tacker database" do
-        connection db_settings[:connection]
-        username "#{db_user}"
-        password "#{db_pass}"
-        database_name "#{db_name}"
-        host '%'
-        privileges db_settings[:privs]
-        provider db_settings[:user_provider]
-        action :grant
-        only_if { !ha_enabled || CrowbarPacemakerHelper.is_cluster_founder?(node) }
-    end
-
+if db == "mysql"
+    include_recipe "tacker::db-mysql"
+    node[@cookbook_name][:db][:sql_connection] = "mysql://tacker:tacker@127.0.0.1/tacker"
+else
+    include_recipe "tacker::db-postgre"
     node[@cookbook_name][:db][db_conn_name] = "#{db_settings[:url_scheme]}://#{db_user}:#{db_pass}@#{db_settings[:address]}/#{db_name}"
     unless sql_address_name.nil?
         node[@cookbook_name][:db][sql_address_name] = sql_address
     end
-
 end
 
 # Registering the service in keystone
@@ -253,6 +208,13 @@ end
 
 template "/etc/tacker/api-paste.ini" do
 	source "api-paste.ini.erb"
+	owner "root"
+	group node[:tacker][:service_group]
+	mode "0640"
+end
+
+template "/root/tackerc" do
+	source "tackerc.erb"
 	owner "root"
 	group node[:tacker][:service_group]
 	mode "0640"
