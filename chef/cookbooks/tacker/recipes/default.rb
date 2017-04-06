ebe5274af56ce5523a49e8ffc053b761071464be
root <root@crowbar.virtual.cloud.suse.de>
2017-04-06 11:25:37 +0000

    Style is fixed
    Signed-off-by: root <root@crowbar.virtual.cloud.suse.de>

--- chef/cookbooks/tacker/recipes/default.rb
+++ chef/cookbooks/tacker/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: opendaylight
+# Cookbook Name: tacker
 # Recipe:: default
 #
 # Copyright 2016, SUSE LINUX Products GmbH
@@ -20,35 +20,41 @@
 ha_enabled = false
 
 git "/tmp/tacker" do
-    repository "https://github.com/trozet/tacker.git"
-    reference "SFC_colorado"
-    action :sync
+  repository "https://github.com/trozet/tacker.git"
+  reference "SFC_colorado"
+  action :sync
 end
 
 git "/tmp/tackerclient" do
-    repository "https://github.com/trozet/python-tackerclient.git"
-    reference "SFC_refactor"
-    action :sync
+  repository "https://github.com/trozet/python-tackerclient.git"
+  reference "SFC_refactor"
+  action :sync
 end
 
 db = "mysql"
 
 if db == "mysql"
-    include_recipe "tacker::db-mysql"
-    node[@cookbook_name][:db][:sql_connection] = "mysql://tacker:tacker@127.0.0.1/tacker"
+  include_recipe "tacker::db-mysql"
+  node[@cookbook_name][:db][:sql_connection] = "mysql://tacker"\
+                                               ":tacker@127.0.0.1/tacker"
 else
-    include_recipe "tacker::db-postgre"
-    node[@cookbook_name][:db][db_conn_name] = "#{db_settings[:url_scheme]}://#{db_user}:#{db_pass}@#{db_settings[:address]}/#{db_name}"
-    unless sql_address_name.nil?
-        node[@cookbook_name][:db][sql_address_name] = sql_address
-    end
+  include_recipe "tacker::db-postgre"
+  node[@cookbook_name][:db][db_conn_name] = "#{db_settings[:url_scheme]}://"\
+                                            "#{db_user}:#{db_pass}@#"\
+                                            "{db_settings[:address]}/"\
+                                            "#{db_name}"
+  unless sql_address_name.nil?
+    node[@cookbook_name][:db][sql_address_name] = sql_address
+  end
 end
 
 # Registering the service in keystone
 
 api_port = node[:tacker][:api][:service_port]
 tacker_protocol = node[:tacker][:api][:protocol]
-my_public_host = CrowbarHelper.get_host_for_public_url(node, node[:tacker][:api][:protocol] == "https", ha_enabled)
+my_public_host = CrowbarHelper.get_host_for_public_url(
+ node, node[:tacker][:api][:protocol] == "https", ha_enabled
+)
 my_admin_host = CrowbarHelper.get_host_for_admin_url(node, ha_enabled)
 
 keystone_settings = KeystoneHelper.keystone_settings(node, @cookbook_name)
@@ -58,178 +64,171 @@ register_auth_hash = { user: keystone_settings["admin_user"],
                        tenant: keystone_settings["admin_tenant"] }
 
 keystone_register "tacker wakeup keystone" do
-	protocol keystone_settings["protocol"]
-    	insecure keystone_settings["insecure"]
-    	host keystone_settings["internal_url_host"]
-    	port keystone_settings["admin_port"]
-    	auth register_auth_hash
-    	action :wakeup
+  protocol keystone_settings["protocol"]
+  insecure keystone_settings["insecure"]
+  host keystone_settings["internal_url_host"]
+  port keystone_settings["admin_port"]
+  auth register_auth_hash
+  action :wakeup
 end
 
 keystone_register "register tacker user" do
-	protocol keystone_settings["protocol"]
-  	insecure keystone_settings["insecure"]
-  	host keystone_settings["internal_url_host"]
-  	port keystone_settings["admin_port"]
-  	auth register_auth_hash
-  	user_name keystone_settings["service_user"]
-  	user_password keystone_settings["service_password"] 
-  	tenant_name keystone_settings["service_tenant"]
-  	action :add_user
+  protocol keystone_settings["protocol"]
+  insecure keystone_settings["insecure"]
+  host keystone_settings["internal_url_host"]
+  port keystone_settings["admin_port"]
+  auth register_auth_hash
+  user_name keystone_settings["service_user"]
+  user_password keystone_settings["service_password"]
+  tenant_name keystone_settings["service_tenant"]
+  action :add_user
 end
 
 keystone_register "give tacker user access" do
-  	protocol keystone_settings["protocol"]
-  	insecure keystone_settings["insecure"]
-  	host keystone_settings["internal_url_host"]
-  	port keystone_settings["admin_port"]
-  	auth register_auth_hash
-  	user_name keystone_settings["service_user"]
-  	tenant_name keystone_settings["service_tenant"]
-  	role_name "admin"
-  	action :add_access
+  protocol keystone_settings["protocol"]
+  insecure keystone_settings["insecure"]
+  host keystone_settings["internal_url_host"]
+  port keystone_settings["admin_port"]
+  auth register_auth_hash
+  user_name keystone_settings["service_user"]
+  tenant_name keystone_settings["service_tenant"]
+  role_name "admin"
+  action :add_access
 end
 
 keystone_register "register tacker service" do
-  	protocol keystone_settings["protocol"]
-  	insecure keystone_settings["insecure"]
-  	host keystone_settings["internal_url_host"]
-  	port keystone_settings["admin_port"]
-  	auth register_auth_hash
-  	service_name "tacker"
-  	service_type "servicevm"
-  	service_description "Openstack Tacker Service"
-  	action :add_service
+  protocol keystone_settings["protocol"]
+  insecure keystone_settings["insecure"]
+  host keystone_settings["internal_url_host"]
+  port keystone_settings["admin_port"]
+  auth register_auth_hash
+  service_name "tacker"
+  service_type "servicevm"
+  service_description "Openstack Tacker Service"
+  action :add_service
 end
 
 keystone_register "register tacker endpoint" do
-  	protocol keystone_settings["protocol"]
-  	insecure keystone_settings["insecure"]
-  	host keystone_settings["internal_url_host"]
-  	port keystone_settings["admin_port"]
-  	auth register_auth_hash
-  	endpoint_service "tacker"
-  	endpoint_region keystone_settings["endpoint_region"]
-  	endpoint_publicURL "#{tacker_protocol}://#{my_public_host}:#{api_port}/"
-  	endpoint_adminURL "#{tacker_protocol}://#{my_admin_host}:#{api_port}/"
-  	endpoint_internalURL "#{tacker_protocol}://#{my_admin_host}:#{api_port}/"
-  	action :add_endpoint_template
+  protocol keystone_settings["protocol"]
+  insecure keystone_settings["insecure"]
+  host keystone_settings["internal_url_host"]
+  port keystone_settings["admin_port"]
+  auth register_auth_hash
+  endpoint_service "tacker"
+  endpoint_region keystone_settings["endpoint_region"]
+  endpoint_publicURL "#{tacker_protocol}://#{my_public_host}:#{api_port}/"
+  endpoint_adminURL "#{tacker_protocol}://#{my_admin_host}:#{api_port}/"
+  endpoint_internalURL "#{tacker_protocol}://#{my_admin_host}:#{api_port}/"
+  action :add_endpoint_template
 end
 
 ## Install the tacker client package
 
 package "install pip" do
-	package_name 'python-pip'
+  package_name "python-pip"
 end
 
 execute "install client" do
-	command "pip install ."
-	cwd "/tmp/tackerclient"
-end 
+  command "pip install ."
+  cwd "/tmp/tackerclient"
+end
 
 ## Install the tacker server package
 
 execute "install server" do
-        command "pip install ."
-        cwd "/tmp/tacker"
+  command "pip install ."
+  cwd "/tmp/tacker"
 end
 
 ## Add tacker group and user
 
-group 'tacker' do
-	action :create
+group "tacker" do
+  action :create
 end
 
-user 'tacker' do
-	comment 'adding tacker user'
-	gid 'tacker'
+user "tacker" do
+  comment "adding tacker user"
+  gid "tacker"
 end
 
 ## Create directories /etc/tacker, /var/log/tacker, /var/lib/tacker
 
-directory '/var/log/tacker' do
-  owner 'tacker'
-  group 'tacker'
-  mode '0750'
+directory "/var/log/tacker" do
+  owner "tacker"
+  group "tacker"
+  mode "0750"
   action :create
 end
 
-directory '/var/lib/tacker' do
-  owner 'root'
-  group 'root'
-  mode '0750'
+directory "/var/lib/tacker" do
+  owner "root"
+  group "root"
+  mode "0750"
   action :create
 end
 
-directory '/etc/tacker' do
-  owner 'root'
-  group 'root'
-  mode '0750'
+directory "/etc/tacker" do
+  owner "root"
+  group "root"
+  mode "0750"
   action :create
 end
-
 
 ## Create /etc/tacker.conf
 heat_protocol = node[:heat][:api][:protocol]
 heat_server = node[:heat][:elements][:'heat-server']
 heat_port = node[:heat][:api][:port]
 
-Chef::Log.info("heat_server: #{heat_server}")
-Chef::Log.info("heat_server: #{heat_server[0]}")
-Chef::Log.info("heat_server: #{heat_port.to_s}")
+heat_uri = heat_protocol + "://" + heat_server[0] + ":" + heat_port.to_s + "/v1"
 
-heat_uri = heat_protocol + "://" + heat_server[0] + ":" + heat_port.to_s + "/v1" 
-
-rabbitmq_settings = CrowbarOpenStackHelper.rabbitmq_settings(node, "tacker")
-
-Chef::Log.info("This is the rabbit_settings: #{rabbitmq_settings}")
-Chef::Log.info("This is the keystone_settings: #{keystone_settings}")
-
+# rabbitmq_settings = CrowbarOpenStackHelper.rabbitmq_settings(node, "tacker")
 
 template "/etc/tacker/tacker.conf" do
-	source "tacker.conf.erb"
-	owner "root"
-	group node[:tacker][:service_group]
-	mode "0640"
-	variables(
-		bind_host: my_admin_host,
-		bind_port: api_port,
-                rabbit_settings: CrowbarOpenStackHelper.rabbitmq_settings(node, "tacker"),
-                keystone_settings: keystone_settings,
-		connection: node[@cookbook_name][:db][:sql_connection],
-		heat_uri: heat_uri
-#		infra_driver=opendaylight
-#		username=admin
-#		ip=192.168.0.2
-#		password=admin
-#		port=8282
-        ) 
+  source "tacker.conf.erb"
+  owner "root"
+  group node[:tacker][:service_group]
+  mode "0640"
+  variables(
+    bind_host: my_admin_host,
+    bind_port: api_port,
+    rabbit_settings: CrowbarOpenStackHelper.rabbitmq_settings(node, "tacker"),
+    keystone_settings: keystone_settings,
+    connection: node[@cookbook_name][:db][:sql_connection],
+    heat_uri: heat_uri
+    # infra_driver=opendaylight
+    # username=admin
+    # ip=192.168.0.2
+    # password=admin
+    # port=8282
+  )
 end
 
 template "/etc/tacker/api-paste.ini" do
-	source "api-paste.ini.erb"
-	owner "root"
-	group node[:tacker][:service_group]
-	mode "0640"
+  source "api-paste.ini.erb"
+  owner "root"
+  group node[:tacker][:service_group]
+  mode "0640"
 end
 
 template "/root/tackerc" do
-	source "tackerc.erb"
-	owner "root"
-	group node[:tacker][:service_group]
-	mode "0640"
-        variables(
-               keystone_settings: keystone_settings 
-        )
+  source "tackerc.erb"
+  owner "root"
+  group node[:tacker][:service_group]
+  mode "0640"
+  variables(
+    keystone_settings: keystone_settings
+  )
 end
 
 ## Start the service
 
 bash "start tacker" do
-	user "root"
-	retries 3
-	retry_delay 2
-    	code <<-EOH
-        	/usr/bin/python /usr/bin/tacker-server --config-file=/etc/tacker/tacker.conf --verbose --log-file=/var/log/tacker/tacker-server.log
-    	EOH
+  user "root"
+  retries 3
+  retry_delay 2
+  code <<-EOH
+    /usr/bin/python /usr/bin/tacker-server \
+    --config-file=/etc/tacker/tacker.conf \
+    --verbose --log-file=/var/log/tacker/tacker-server.log
+  EOH
 end
